5802c86384ab5b74de1e579506a241725865858c
thevinodpatidar <pvinod813@gmail.com>
2025-10-28 16:03:59 +0530

    Refactor imports in page.tsx for improved readability by rearranging the order of Image and React hooks.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Image from "next/image";
-import { useState, useEffect } from "react";
 import vinodImage from "@/public/vinod-patidar.jpg";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [activeSection, setActiveSection] = useState("home");
